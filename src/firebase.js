5d30486ea3174b4c10123cd809b0d55236410628
Luca Gamerro <43759024+lucagamerro@users.noreply.github.com>
2021-01-30 19:32:57 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCuBSyZaDPEa2dFS1djn-0mrj0nF-3NEFc",
-    authDomain: "luke-moneybox-42223.firebaseapp.com",
-    projectId: "luke-moneybox-42223",
-    storageBucket: "luke-moneybox-42223.appspot.com",
-    messagingSenderId: "103263825371",
-    appId: "1:103263825371:web:0d9d1cf50cd3254a9a4bfe"
+    // Firebase config
 };
 
 firebase.initializeApp(firebaseConfig);
